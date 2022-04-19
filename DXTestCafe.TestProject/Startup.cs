1c0b43f2e0e9286a1244251663d1add941548eb2
Don Wibier <don@anykey.nl>
2022-04-19 15:32:34 +0200

    Upgraded to latest DX version and CORE 6

--- DXTestCafe.TestProject/Startup.cs
+++ DXTestCafe.TestProject/Startup.cs
@@ -30,13 +30,15 @@ namespace DXTestCafe.TestProject
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddDbContext<ApplicationDbContext>(options =>
+			services.AddDbContext<ApplicationDbContext>(options => 
 				options.UseSqlServer(
-					Configuration.GetConnectionString("DefaultConnection")));
+					Configuration.GetConnectionString("DefaultConnection"))				
+			);
+			
 			services.AddDatabaseDeveloperPageExceptionFilter();
 			services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
 				.AddRoles<IdentityRole>()
-				.AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<ApplicationDbContext>();
 			// Add framework services.
 			services
 				.AddRazorPages()
